da1306ed4fc45731bb5e370bdf59d0f40e9159e1
Bernhard Kühnel <bkuehnel@proficom.de>
2022-06-20 14:48:23 +0200

    run report from batch runner

---
+++ Tag 1 - Capture+Replay/Report/Report/Resources/CheckPoints/1f72c237-8099-4166-9988-2477ba95343a/o.js
@@ -0,0 +1,3 @@
+var orginalFile = {
+	content : "Advantage Shopping   DataTable(\"FileContent_http\", dtGlobalSheet)\r\n\r\n\r\n\r\n\r\n1 of 2                                  \\d\\d/\\d\\d/2022, \\d\\d:\\d\\d\r\nAdvantage Shopping   http://advantageonlineshopping.com/#/\r\n\r\n\r\n\r\n\r\n2 of 2                                  15/06/2022, 11:11\r\n"
+}
